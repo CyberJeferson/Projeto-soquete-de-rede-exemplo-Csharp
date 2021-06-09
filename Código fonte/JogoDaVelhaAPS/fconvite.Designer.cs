c2b1744132f78858f8827edfcc9b11e795228ffd
JEFERSON OLIVEIRA <63682724+CyberJeferson@users.noreply.github.com>
2021-06-09 18:37:55 -0300

    Projeto ok

---
+++ Código fonte/JogoDaVelhaAPS/fconvite.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace JogoDaVelhaAPS
+{
+    partial class fconvite
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fconvite));
+            this.pFoto = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btnAceitar = new System.Windows.Forms.Button();
+            this.btnRecusar = new System.Windows.Forms.Button();
+            this.lNome = new System.Windows.Forms.Label();
+            this.Verificador = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pFoto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pFoto
+            // 
+            this.pFoto.BackColor = System.Drawing.Color.Black;
+            this.pFoto.Location = new System.Drawing.Point(303, 56);
+            this.pFoto.Name = "pFoto";
+            this.pFoto.Size = new System.Drawing.Size(132, 158);
+            this.pFoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pFoto.TabIndex = 0;
+            this.pFoto.TabStop = false;
+            
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Jokerman", 24F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(162, 286);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(407, 47);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CONVITE DE BATALHA";
+         
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(438, 240);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(129, 153);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+           
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox3.Location = new System.Drawing.Point(151, 243);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(129, 153);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+           
+            // 
+            // btnAceitar
+            // 
+            this.btnAceitar.BackColor = System.Drawing.Color.Black;
+            this.btnAceitar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAceitar.ForeColor = System.Drawing.Color.GreenYellow;
+            this.btnAceitar.Location = new System.Drawing.Point(218, 336);
+            this.btnAceitar.Name = "btnAceitar";
+            this.btnAceitar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceitar.TabIndex = 4;
+            this.btnAceitar.Text = "ACEITAR";
+            this.btnAceitar.UseVisualStyleBackColor = false;
+            this.btnAceitar.Click += new System.EventHandler(this.btnAceitar_Click);
+            // 
+            // btnRecusar
+            // 
+            this.btnRecusar.BackColor = System.Drawing.Color.Black;
+            this.btnRecusar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRecusar.ForeColor = System.Drawing.Color.Red;
+            this.btnRecusar.Location = new System.Drawing.Point(419, 336);
+            this.btnRecusar.Name = "btnRecusar";
+            this.btnRecusar.Size = new System.Drawing.Size(75, 23);
+            this.btnRecusar.TabIndex = 5;
+            this.btnRecusar.Text = "RECUSAR";
+            this.btnRecusar.UseVisualStyleBackColor = false;
+            this.btnRecusar.Click += new System.EventHandler(this.btnRecusar_Click);
+            // 
+            // lNome
+            // 
+            this.lNome.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lNome.AutoSize = true;
+            this.lNome.BackColor = System.Drawing.Color.Black;
+            this.lNome.Font = new System.Drawing.Font("Microsoft Himalaya", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lNome.ForeColor = System.Drawing.Color.Red;
+            this.lNome.Location = new System.Drawing.Point(371, 217);
+            this.lNome.Name = "lNome";
+            this.lNome.Size = new System.Drawing.Size(139, 64);
+            this.lNome.TabIndex = 6;
+            this.lNome.Text = "mikex";
+            // 
+            // Verificador
+            // 
+            this.Verificador.Enabled = true;
+            this.Verificador.Interval = 1000;
+       
+            // 
+            // fconvite
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = global::JogoDaVelhaAPS.Properties.Resources.kisspng_flame_off_the_hook_barbecue_fire_restaurant_15_grill_flames_png_for_free_download_on_mbtskouds_5bfe0edd63fb38_6822048515433766054095;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(739, 362);
+            this.Controls.Add(this.lNome);
+            this.Controls.Add(this.btnRecusar);
+            this.Controls.Add(this.btnAceitar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pFoto);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "fconvite";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "fconvite";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.SystemColors.ActiveCaption;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.fconvite_FormClosing);
+            this.Load += new System.EventHandler(this.fconvite_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pFoto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pFoto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnAceitar;
+        private System.Windows.Forms.Button btnRecusar;
+        private System.Windows.Forms.Label lNome;
+        internal System.Windows.Forms.Timer Verificador;
+    }
+}
---
+++ Código fonte/JogoDaVelhaAPS/frmOcorrencias.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;//Biblioteca padrão do C# usada no projeto para Criar variaveis em String e tratamento de erro em catch
+using MySql.Data.MySqlClient;//Biblioteca do Mysql do oracle, para linguagens .NET
+
+namespace JogoDaVelhaAPS
+{
+    public partial class frmOcorrencias : Form      //classe frmOcorrencias 
+    {
+        public frmOcorrencias()//Método construtor da classe
+        {
+            InitializeComponent();
+        }
+        Cliente cliente = new Cliente();//Novo objeto do tipo cliente
+        private String Verificar;//Novo objeto do tipo string
+        private void frmOcorrencias_Load(object sender, EventArgs e)//Esse método é um evento que ocorre ao carregar o formulario
+        {
+            cMapa.LimparMapa();//Limpa o mapa
+            cMapa.CarregarMapa(pMapa);//Adiciona o mapa no painel
+          
+            txtTipo.SelectedIndex = 0;//Inicia o combobox txtTipo no indice 0
+          
+            CarregarOcorrenciasNaTabela();//Adiciona as ocorrencias do banco de dados no datagrid, e coloca no mapa
+            timer1.Enabled = true;// Ativa o timer 1
+           
+        
+           
+
+        }
+
+        private void frmOcorrencias_FormClosing(object sender, FormClosingEventArgs e)//Método usado quando o formulário é fechado
+        {
+            Program.FecharTudo(true);//Se o fofmulario bome for fechado encerra toda aplicacão
+            Program.AbriHome();////Abre o formulario Home      
+            pMapa.Controls.Clear();//Remove o mapa dos controles do painel pMapa
+
+        }
+
+        private void pMapa_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        double v;
+        private void timer1_Tick(object sender, EventArgs e)//Metodo usado para obter a velocidade de movimento no gps e atualizar a posição no gps ele é um evento de timer e será disparado a cada 500 ms
+        {
+            cMapa.obterCordeenadas();//Atualiza a posição no gps
+            v = cMapa.obtervelocidade() * 3.6;//Obtém a velocidade do movimento na geolocalização
+         
+          
+            if (v == 0)//Se a velocidade for 0  então:
+            {
+                Velocidade.Text = "0 km/h";//Vai exibir na tela a velocidade 0 seguida de km/h
+            }
+            else //Se for diferente de 0
+            {
+                Velocidade.Text = v.ToString() + " km/h";//Vai exibir na tela a velocidade seguido de km/h
+            }
+         
+         
+
+
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)//Evento click do label 8, ao clicar no label ele vai exibir o mapa em modo terreno ou satélite
+        {
+
+            
+            if (label8.Text != "Mapa Terreno")//Se o label estiver exibindo o texto Mapa terreno então:
+            {
+                cMapa.MapaSatelite();//Exibe o mapa como satelite.
+                pMapa.Invalidate();//Invalida o mapa para ser redesenhado na tela
+                label8.Text = "Mapa Terreno";//Muda o texto Para mapa terreno.
+                
+            }
+            else//Se não estiver exibindo o texto Mapa Terreno
+            {
+                cMapa.MapaTerreno();//Exibe o mapa como terreno
+                pMapa.Invalidate();//Invalida para ser redesenhado na tela
+                label8.Text = "Mapa Satélite";//Exibe o texto Mapa Satélite na label
+            }
+        }
+
+
+
+        private void latlong_Tick(object sender, EventArgs e)//Esse método também é um evento de timer que será disparado a cada 500 ms, ele vai verificar comandos do servidor soquete de acordo com o comando vai carregar informações do banco de dados de novas ocorrencias no mapa e na tabela do datagrid, ele também vai colocar as posições do mapa de latitude e longitude em dois textbox, ele também anima as imagens dos incones do mapa
+        {
+            Combate.acoes(Cliente.getMenssagemServidor());//Verifica os comando recebidos do servidor soquete
+            if (Combate.podeRecarregarTabela())//Se a boleana podeRecarregarTabela() for verdade então:
+            {
+                gOcorrencia.Rows.Clear();//Limpa a tabela do datagrid
+                CarregarOcorrenciasNaTabela();//Carrega as novas informações do banco de dados no datagrid e no mapa
+              
+              
+                Combate.RecarregaTabela(false);//Muda a boleana recebida de combate para false
+            }
+            txtlat.Text = cMapa.getMapaPositionLat();//Coloca a latitude no textbox txtlat
+            txtlong.Text = cMapa.getMapaPositionLong();//Coloca a longitude no textbox txtlong
+            cMapa.mudarImagem();//Anima as imagens de cMapa, fazendo elas mudarem 
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)//Método event click do botão de criar ocorrência
+        {
+            dao.InserirOcorrencia(txtTipo.Text,txtdesc.Text,txtlat.Text,txtlong.Text);//Insere no banco de dados as informações
+            Verificar =  "alerta " + txtTipo.Text + " " + txtdesc.Text;//Verificar recebe essas informações
+            cliente.setEnviarMensagem(Verificar);//A variavel de enviar dados ao servidor soquete recebe verificar
+            Cliente.buffar(); //Envia a informação ao servidor soquete          
+            Cliente.desbufar();//Para a thread
+            Combate.VerificarmensagemEnviada(Verificar);//Verifica se a mensagem foi enviada
+            txtTipo.SelectedIndex = 0;//Mua o indice do combobox para 0
+            txtdesc.Text = "";//Limpa o textbox descrição
+           
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)//Botão voltar ele fecha o formulário
+        {
+ 
+            this.Close();//Fecha o formulário
+        }
+        private static MySqlConnection Conexao;//Cria um novo objeto do tipo MySqlConnection 
+        private static MySqlCommand mComando;//Cria um novo objeto do tipo MySqlCommand
+        static MySqlDataReader dtreader;//Cria um novo objeto do tipo MySqlDataReader
+        String[] linhaOcorrencias;//Nova array do tipo string
+        String Latitude, Longitude, Descricao,Tipo,Nomeo, auxLatitude, auxLongitude;//Novos objetos do tipo string
+        int Linhas;//Novo objeto do tipo inteiro
+        String EnviarCordeenadas;//Novo objeto do tipo String
+        private void EnviarCords_Tick(object sender, EventArgs e)
+        {
+           
+            EnviarCordeenadas = ">" + Combate.getMeuId() + cMapa.ObterCordenadasGPS();
+            cliente.setEnviarMensagem(EnviarCordeenadas);
+            Cliente.buffar();
+
+            
+        }
+
+
+        private void gOcorrencia_CellClick(object sender, DataGridViewCellEventArgs e)//Esse método é um evento CellClick do datagrid onde exibe a ocorrência clicada no mapa
+        {
+            Linhas = gOcorrencia.CurrentCell.RowIndex;//Recebe a linha selecionada do datagridview.
+            auxLatitude = gOcorrencia.Rows[Linhas].Cells[2].Value.ToString();//Recebe a latititude da tabela
+            auxLongitude = gOcorrencia.Rows[Linhas].Cells[3].Value.ToString();//Recebe a longitude da tabela
+            cMapa.DirecionarMapa(Double.Parse(auxLatitude),Double.Parse(auxLongitude));//Exibe a latitude e alongitude onde se encontra a ocorrencia no mapa e dar um zoom para ver de perto
+
+        }
+
+        private void CarregarOcorrenciasNaTabela()//Método para carregar as ocorrencias do banco de dados. coloca no mapa e no data grid
+        {
+            try
+            {
+
+                Conexao = dao.Conectar();//Conecta ao banco de dados
+
+                mComando = new MySqlCommand();//mComando é um novo objeto do tipo MySqlCommand
+                mComando.Connection = Conexao;//Recebe a conexão com o bando de dados
+                mComando.CommandText = "SELECT * FROM ocorrencia";
+
+                dtreader = mComando.ExecuteReader();//Recebe os dados em binario
+                Nomeo = "";
+                while (dtreader.Read())//Enquanto estiver lendo os dados recebidos
+                {
+                    Tipo = dtreader["tipo_ocorrencia"].ToString();//Recebe o tipo de ocorrencia
+                    Nomeo = dtreader["cd_ocorrencia"].ToString();//Recebe o código da ocorrencia
+                    Descricao = dtreader["ds_ocorrencia"].ToString();//Recebe a descrição 
+                    Latitude = dtreader["latitude"].ToString();//Recebe a latitude
+                    Longitude = dtreader["longetude"].ToString();//Recebe a longitude
+                    linhaOcorrencias = new String[] { Tipo, Descricao, Latitude, Longitude };//A array recebe as informações da ocorrencia
+                    gOcorrencia.Rows.Add(linhaOcorrencias);//E as informações são adicionadas no datagrid
+      
+                    if (Nomeo != "")//Se o id da ocorrencia for diferente de vazio
+                    {
+                        cMapa.novaMarcacao(Nomeo, "./ocorrencia.png", Double.Parse(Latitude), Double.Parse(Longitude));//Coloca a ocorrencia no mapa
+                    }
+                  
+
+
+
+                }
+                Conexao.Close();//Fecha a conexão com o banco de dados
+            }
+            catch (Exception)
+            {
+               //Em caso de erro não fazer nada
+            }
+
+        }
+     
+  
+    }
+}
